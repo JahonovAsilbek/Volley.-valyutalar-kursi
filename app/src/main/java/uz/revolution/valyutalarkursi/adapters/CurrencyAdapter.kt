92224d39b615c13048175a162982a22fe95e6ea3
JahonovAsilbek <asilbekjahonov2001@gmail.com>
2021-05-08 01:25:46 +0500

    Hello World!

---
+++ app/src/main/java/uz/revolution/valyutalarkursi/adapters/CurrencyAdapter.kt
@@ -0,0 +1,48 @@
+package uz.revolution.valyutalarkursi.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import uz.revolution.valyutalarkursi.databinding.ItemCurrencyBinding
+import uz.revolution.valyutalarkursi.models.Currency
+
+class CurrencyAdapter : RecyclerView.Adapter<CurrencyAdapter.VH>() {
+
+    private var onItemClick: OnItemClick? = null
+    private var list: List<Currency>? = null
+
+    fun setAdapter(list: List<Currency>) {
+        this.list = list
+    }
+
+    inner class VH(var binding: ItemCurrencyBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun onBind(currency: Currency) {
+            binding.name.text = currency.CcyNm_UZ
+
+            binding.root.setOnClickListener {
+                if (onItemClick != null) {
+                    onItemClick?.onClick(currency)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        return VH(ItemCurrencyBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.onBind(list!![position])
+    }
+
+    override fun getItemCount(): Int = list!!.size
+
+    interface OnItemClick {
+        fun onClick(currency: Currency)
+    }
+
+    fun setOnItemClick(onItemClick: OnItemClick) {
+        this.onItemClick = onItemClick
+    }
+}
